34b92efa0f8f14a27aefa87b643d4c7fb1110d6d
Abdullah_Sheikh <h.abdullahsheikh568@gmail.com>
2022-12-10 22:43:20 +0500

    Assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+let NumOne = await inquirer.prompt([{
+        name: "numberOne",
+        type: "number",
+        message: "First parameter"
+    }]);
+// console.log(chalk.red(NumOne.numberOne));
+let NumTwo = await inquirer.prompt([{
+        name: "numberTwo",
+        type: "number",
+        message: "Second parameter"
+    }]);
+// console.log(chalk.green(NumTwo.numberTwo));
+let Operator = await inquirer.prompt([{
+        name: "OperatorSign",
+        type: "string",
+        message: "Select Your Operator (+ - / x)"
+    }]);
+console.log(Calculate(Operator.OperatorSign));
+function Calculate(Operator) {
+    switch (Operator) {
+        case "+":
+            return parseInt(NumOne.numberOne) + parseInt(NumTwo.numberTwo);
+        case "-":
+            return NumOne.numberOne - NumTwo.numberTwo;
+        case "X":
+            return NumOne.numberOne * NumTwo.numberTwo;
+        case "*":
+            return NumOne.numberOne * NumTwo.numberTwo;
+        case "/":
+            return chalk.blue(NumOne.numberOne / NumTwo.numberTwo);
+    }
+}
